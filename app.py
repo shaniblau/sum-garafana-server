0d4507341d93cea4afb7652706b507f502d372a4
shaniblau <blaushani02@gmail.com>
2023-08-24 14:29:38 +0300

    changed tests according to code changes

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask, Response, request
+import logging as log
+from fastapi import FastAPI
 
-app = Flask(__name__)
+app = FastAPI()
 
+log_format = '%(asctime)s ,%(message)s'
+log.basicConfig(filename='alerts.log', format=log_format)
+logger = log.getLogger('app_logger')
 
-@app.route('/webhook', methods=['POST'])
-def get_webhook():
-    print(request.json)
-    return Response(status=200)
+@app.post('/webhook')
+async def get_webhook(alert):
+    logger.info(f'alert received, reason: {alert.json()}')
+    print(alert.json())
+    return {"status": "success"}
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from app import app
+import uvicorn
 
 if __name__ == "__main__":
-    FLASK_APP=app
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
