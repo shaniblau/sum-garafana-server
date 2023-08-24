1b19eb428a2e7a290b217ab51f0ba5c691180ad4
shaniblau <blaushani02@gmail.com>
2023-08-24 13:35:51 +0300

    changed tests according to code changes

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import logging as log
+
+app = Flask(__name__)
+log_format = '%(asctime)s ,%(message)s'
+log.basicConfig(filename='alerts.log', format=log_format)
+logger = log.getLogger('app_loger')
+
+
+@app.route('/webhook', methods=['POST'])
+def get_webhook(alert):
+    logger.info(f'alert received, reason: {alert.json}')
+    print(alert.json)
