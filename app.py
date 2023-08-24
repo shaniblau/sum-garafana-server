5bd4df571b5ffb356e01d827249a9259375448f2
shaniblau <blaushani02@gmail.com>
2023-08-24 14:32:07 +0300

    changed tests according to code changes

--- app.py
+++ app.py
@@ -7,8 +7,9 @@ log_format = '%(asctime)s ,%(message)s'
 log.basicConfig(filename='alerts.log', format=log_format)
 logger = log.getLogger('app_logger')
 
+
 @app.post('/webhook')
-async def get_webhook(alert):
-    logger.info(f'alert received, reason: {alert.json()}')
-    print(alert.json())
+async def get_webhook(alert: dict):
+    logger.info(f'Alert received: {alert}')
+    print(alert)
     return {"status": "success"}
